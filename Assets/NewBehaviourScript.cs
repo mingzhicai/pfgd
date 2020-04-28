2dcf376f110358dc1e63dcccc9b82d477970d32d
Mingzhi Cai <minztsai96@gmail.com>
2020-04-28 14:39:46 -0400

    Initial commit

---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject block;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision targetObj)
+    {
+        if (targetObj.gameObject.tag == "sphere")
+        {
+            Destroy(targetObj.gameObject);
+        }
+    }
+
+
+
+}

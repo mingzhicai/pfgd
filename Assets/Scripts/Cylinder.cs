32c79d9f357aa45de30ce26e1892725a80c9a3fd
Mingzhi Cai <minztsai96@gmail.com>
2020-04-28 23:06:50 -0400

    firstplayable

---
+++ Assets/Scripts/Cylinder.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cylinder : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public GameObject button;
+
+    void Start()
+    {
+
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        button.transform.Translate(0,-Time.deltaTime,0);
+    }
+}
